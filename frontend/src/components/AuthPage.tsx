17c418172008d50e666e160ebd0e510f6716a112
Ricardo Cardoso <alf4rd@gmail.com>
2026-08-19 19:16:06 -0300

    new edition

--- frontend/src/components/AuthPage.tsx
+++ frontend/src/components/AuthPage.tsx
@@ -15,7 +15,7 @@ export const AuthPage: React.FC<AuthPageProps> = ({ onSuccess }) => {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const API_URL = import.meta.env.API_URL || 'http://localhost:5000';
+  const API_URL = import.meta.env.API_URL || 'http://krokante:5000';
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -23,11 +23,11 @@ export const AuthPage: React.FC<AuthPageProps> = ({ onSuccess }) => {
     setLoading(true);
 
     const url = isLogin 
-      ? `${API_URL}/api/auth/login`
-      : `${API_URL}/api/auth/signup`;
+      //? `${API_URL}/api/auth/login`
+      //: `${API_URL}/api/auth/signup`;
     
-      //? '/api/auth/login'
-      //: '/api/auth/signup';
+      ? '/api/auth/login'
+      : '/api/auth/signup';
     
       //? 'http://localhost:5000/api/auth/login' 
       //: 'http://localhost:5000/api/auth/signup';
