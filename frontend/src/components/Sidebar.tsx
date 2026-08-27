e5d361b02c8415a8fe20953baf814acb1013517c
cardosokks <cardosokks@gmail.com>
2026-08-27 01:21:00 -0300

    fix: remoção de syntax error TS1381 no Sidebar.tsx

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -519,7 +519,6 @@ export const Sidebar: React.FC<SidebarProps> = ({
             ))}
           </div>
         </div>
-      )}
 
       {showNewTemplateModal && (
         <div className="fixed inset-0 z-50 bg-black/80 backdrop-blur-sm flex items-center justify-center p-4">
