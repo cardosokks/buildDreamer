2ccdfbc1b69dc1dcfa10055a1de13797f58ae7ae
cardosokks <cardosokks@gmail.com>
2026-08-29 13:57:35 -0300

    redefined vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,15 +7,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5005',
+        target: 'http://localhost:5000',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:5005',
+        target: 'http://localhost:5000',
         changeOrigin: true
       },
       '/socket.io': {
-        target: 'http://localhost:5005',
+        target: 'http://localhost:5000',
         ws: true
       }
     }
